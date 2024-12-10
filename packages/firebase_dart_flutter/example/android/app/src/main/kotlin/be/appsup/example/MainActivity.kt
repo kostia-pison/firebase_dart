f43e96e6073c779aecf0d8958ed9adfd220fe904
rikbellens <rik.bellens@appsup.be>
2024-12-10 15:05:37 +0100

    test: update android project for example app

--- packages/firebase_dart_flutter/example/android/app/src/main/kotlin/be/appsup/example/MainActivity.kt
+++ packages/firebase_dart_flutter/example/android/app/src/main/kotlin/be/appsup/example/MainActivity.kt
@@ -2,5 +2,4 @@ package be.appsup.firebase_dart_flutter_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
