dc6e3b2708e96075e0b862e75cc75c7a9cd4d7b7
rikbellens <rik.bellens@appsup.be>
2024-12-10 14:15:01 +0100

    test: update ios project for example app

--- packages/firebase_dart_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/firebase_dart_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,19 @@
 import FlutterMacOS
 import Foundation
 
+import facebook_auth_desktop
+import flutter_secure_storage_macos
+import google_sign_in_ios
 import package_info
-import path_provider_macos
+import path_provider_foundation
 import sign_in_with_apple
 import uni_links_desktop
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
