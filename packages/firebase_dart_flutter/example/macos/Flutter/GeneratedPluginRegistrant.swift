f88f5cfdbeaf9bbc81524a901980a7fb7a0df0ba
Cody Goldberg <cody@goldberg.fyi>
2025-01-07 09:34:13 -0500

    generated swift changes to reflect facebook removal

--- packages/firebase_dart_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/firebase_dart_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import facebook_auth_desktop
-import flutter_secure_storage_macos
+import app_links
 import google_sign_in_ios
 import package_info
 import path_provider_foundation
@@ -14,8 +13,7 @@ import sign_in_with_apple
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
