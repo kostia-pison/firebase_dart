35a3dbb8335946e4f245c0622dd11dd3266e759e
kostia-pison <kostiantyn.rybalka@pison.com>
2025-06-20 19:42:12 +0200

    Update gapi

--- packages/firebase_dart/lib/src/auth/iframeclient/gapi.dart
+++ packages/firebase_dart/lib/src/auth/iframeclient/gapi.dart
@@ -1,7 +1,7 @@
 @JS('gapi')
 library gapi;
 
-import 'package:js/js.dart';
+import 'dart:js_interop';
 
 @JS()
 external void load(String libraries, LoadConfig config);
@@ -9,8 +9,13 @@ external void load(String libraries, LoadConfig config);
 @JS()
 @anonymous
 abstract class LoadConfig {
-  external factory LoadConfig(
-      {Function callback, Function onerror, num timeout, Function ontimeout});
+  external factory LoadConfig({
+    Function callback,
+    Function onerror,
+    num timeout,
+    Function ontimeout,
+  });
+
   external Function get callback;
   external Function get onerror;
   external num get timeout;
